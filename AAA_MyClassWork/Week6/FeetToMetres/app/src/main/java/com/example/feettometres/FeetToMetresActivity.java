98e8ec9d3ec31fc97fc68ee984fbc56afb6a76d6
Kieron Gillingham <4gillk91@solent.ac.uk>
2020-02-05 10:55:43 +0000

    basic exceptions task

---
+++ AAA_MyClassWork/Week6/FeetToMetres/app/src/main/java/com/example/feettometres/FeetToMetresActivity.java
@@ -0,0 +1,40 @@
+package com.example.feettometres;
+
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class FeetToMetresActivity extends AppCompatActivity implements OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        TextView tv = (TextView)findViewById(R.id.tv2);
+        EditText et = (EditText)findViewById(R.id.et1);
+
+        try {
+            double feet = Double.parseDouble("five");
+            //double feet = Double.parseDouble(et.getText().toString());
+            double metres = feet*0.305;
+
+            tv.setText("In metres that is: " + metres);
+
+        } catch (NumberFormatException e) {
+            new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage("Not a number!").show();
+        } catch (Exception e) {
+            new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage("Please enter a value to convert.").show();
+        }
+    }
+}
