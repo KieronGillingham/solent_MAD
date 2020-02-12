e772a3501f12ea805d96fd164421701ef68a8ddf
Kieron Gillingham <4gillk91@solent.ac.uk>
2020-02-12 10:16:31 +0000

    text editor init

---
+++ AAA_MyClassWork/Week6/TextEditor/app/src/main/java/com/example/texteditor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.texteditor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.save) {
+            // Save
+            return true;
+        } else if (item.getItemId() == R.id.load) {
+            // Load
+            return true;
+        }
+        return false;
+    }
+}
