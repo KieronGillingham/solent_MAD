40fe12432bc2cf8b58a54cc6051f496b477870bb
Kieron Gillingham <4gillk91@solent.ac.uk>
2020-02-18 11:41:15 +0000

    saving text file locally

--- AAA_MyClassWork/Week6/TextEditor/app/src/main/java/com/example/texteditor/MainActivity.java
+++ AAA_MyClassWork/Week6/TextEditor/app/src/main/java/com/example/texteditor/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.texteditor;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,12 +26,19 @@ public class MainActivity extends AppCompatActivity {
     EditText textPage;
     File root = android.os.Environment.getExternalStorageDirectory();
 
+    int permissionRequestCode = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textPage = findViewById(R.id.text_page);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, permissionRequestCode);
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,14 +50,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.save) {
             try {
-                File location = new File (root.getAbsolutePath() + "/texteditor");
-                location.mkdirs();
-                File file = new File(location, "textedit.txt");
-
-                FileOutputStream f = new FileOutputStream(file);
-
-                PrintWriter pw = new PrintWriter(f);
-
+                PrintWriter pw = new PrintWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/output.txt");
                 pw.println(textPage.getText().toString());
                 pw.flush();
                 pw.close(); // close the file to ensure data is flushed to file
@@ -57,9 +63,43 @@ public class MainActivity extends AppCompatActivity {
             }
 
         } else if (item.getItemId() == R.id.load) {
-            // Load
-            return true;
+            try {
+                textPage.setText("");
+
+                BufferedReader reader = new BufferedReader( new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath() + "/output.txt"));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    textPage.append(line);
+                }
+                new AlertDialog.Builder(this).setMessage("Save successful.").setPositiveButton("Ok", null).show();
+                return true;
+            } catch (IOException e) {
+                new AlertDialog.Builder(this).setMessage("IOException: " + e.toString()).setPositiveButton("Ok", null).show();
+            } catch (Exception e) {
+                new AlertDialog.Builder(this).setMessage("Exception: " + e.toString()).setPositiveButton("Ok", null).show();
+            }
         }
         return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            new AlertDialog.Builder(this).setMessage("Permissions were not successfully set.").setPositiveButton("Ok", null).show();
+                            return;
+                        }
+                    }
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
