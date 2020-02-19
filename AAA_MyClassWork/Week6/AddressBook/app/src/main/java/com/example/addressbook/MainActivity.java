a0d46c33b9f517c3758bd2964129217871cf036f
Kieron Gillingham <4gillk91@solent.ac.uk>
2020-02-19 10:53:20 +0000

    save contacts init

--- AAA_MyClassWork/Week6/AddressBook/app/src/main/java/com/example/addressbook/MainActivity.java
+++ AAA_MyClassWork/Week6/AddressBook/app/src/main/java/com/example/addressbook/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.addressbook;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,12 +15,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Contact> contacts;
     EditText firstNameField, lastNameField, phoneNumberField, addressField;
+    File outputFile = null;
+    int permissionRequestCode = 0;
 
     public class Contact {
         String firstName, lastName, contactNumber, address;
@@ -70,6 +81,36 @@ public class MainActivity extends AppCompatActivity {
         lastNameField = findViewById(R.id.etx_last_name);
         phoneNumberField = findViewById(R.id.etx_phone_number);
         addressField = findViewById(R.id.etx_address);
+
+        String outputFileName = "contacts.csv";
+        outputFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + outputFileName);
+
+        // Request permissions from user if they are not already set
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, permissionRequestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            new AlertDialog.Builder(this).setMessage("Permissions were not successfully set. App will now close.").setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    finish();
+                                }
+                            }).show();
+                        }
+                    }
+                }
+                return;
+            }
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,25 +119,18 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.add) {
-
             addNewContact();
-            return true;
 
-            /*
-            try {
-                PrintWriter pw = new PrintWriter(outputFile.getAbsolutePath());
-                pw.println(textPage.getText().toString());
-                pw.flush();
-                pw.close(); // close the file to ensure data is flushed to file
-                new AlertDialog.Builder(this).setMessage("Save successful.").setPositiveButton("Ok", null).show();
-                return true;
-            } catch (IOException e) {
-                new AlertDialog.Builder(this).setMessage("IOException: " + e.toString()).setPositiveButton("Ok", null).show();
-            } catch (Exception e) {
-                new AlertDialog.Builder(this).setMessage("Exception: " + e.toString()).setPositiveButton("Ok", null).show();
-            } */
+        } else if (item.getItemId() == R.id.save) {
+            saveAllContacts();
+
+        } else {
+            return false;
+
+
 
         /* } else if (item.getItemId() == R.id.load) {
             try {
@@ -122,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
             } */
 
         }
-        return false;
+        return true;
     }
 
     void addNewContact() {
@@ -151,7 +185,30 @@ public class MainActivity extends AppCompatActivity {
             }
             Contact newContact = new Contact(firstNameField.getText().toString(), lastNameField.getText().toString(), phoneNumberField.getText().toString(), addressField.getText().toString());
             contacts.add(newContact);
-        }
+            new AlertDialog.Builder(this).setMessage(newContact.getFirstName() + " " + newContact.getLastName() + " added to contacts. There are now " + contacts.size() + " contacts saved.").setPositiveButton("Ok", null).show();
 
+        }
+    }
+
+    void saveAllContacts() {
+        try {
+            if (contacts.size() > 0) {
+                PrintWriter pw = new PrintWriter(outputFile.getAbsolutePath());
+
+                for (Contact con : contacts) {
+                    pw.println(con.getFirstName() + "," + con.getLastName() + "," + con.getContactNumber() + "," + con.getAddress());
+                }
+                pw.flush();
+                pw.close();
+                new AlertDialog.Builder(this).setMessage("Save successful.").setPositiveButton("Ok", null).show();
+
+            } else {
+                new AlertDialog.Builder(this).setMessage("No contacts to save.").setPositiveButton("Ok", null).show();
+            }
+        } catch (IOException e) {
+            new AlertDialog.Builder(this).setMessage("IOException: " + e.toString()).setPositiveButton("Ok", null).show();
+        } catch (Exception e) {
+            new AlertDialog.Builder(this).setMessage("Exception: " + e.toString()).setPositiveButton("Ok", null).show();
+        }
     }
 }
