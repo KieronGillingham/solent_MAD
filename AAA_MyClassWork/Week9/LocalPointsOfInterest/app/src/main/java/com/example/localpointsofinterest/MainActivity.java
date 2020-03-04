44c135b585fd705b51f26d4872e197f605714068
Kieron Gillingham <4gillk91@solent.ac.uk>
2020-03-04 09:05:18 +0000

    southampton city center poi

--- AAA_MyClassWork/Week9/LocalPointsOfInterest/app/src/main/java/com/example/localpointsofinterest/MainActivity.java
+++ AAA_MyClassWork/Week9/LocalPointsOfInterest/app/src/main/java/com/example/localpointsofinterest/MainActivity.java
@@ -6,8 +6,18 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 
 import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    MapView mv;
+    ItemizedIconOverlay<OverlayItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
         setContentView(R.layout.activity_main);
+
+        // Set default position for map
+        mv = (MapView) findViewById(R.id.mainMap);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+
+        items = new ItemizedIconOverlay<>(this, new ArrayList<OverlayItem>(), null);
+        OverlayItem southamptonCC = new OverlayItem("Southampton City Centre", "City Centre in Southampton", new GeoPoint(50.9014, -1.4041));
+        items.addItem(southamptonCC);
+        mv.getOverlays().add(items);
     }
 }
