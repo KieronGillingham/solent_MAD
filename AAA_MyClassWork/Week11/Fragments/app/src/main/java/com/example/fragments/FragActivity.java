7a6f44c6eeebc82f9218fd3b0b96bb54d8856e59
Kieron Gillingham <4gillk91@solent.ac.uk>
2020-03-11 09:36:34 +0000

    single fragment with public method

--- AAA_MyClassWork/Week11/Fragments/app/src/main/java/com/example/fragments/FragActivity.java
+++ AAA_MyClassWork/Week11/Fragments/app/src/main/java/com/example/fragments/FragActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,14 @@ public class FragActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get fragment from manager
+        FragmentManager fm = getFragmentManager();
+        PersonDetailsFragment personDetailsFrag;
+        personDetailsFrag = (PersonDetailsFragment) fm.findFragmentById(R.id.personDetailsFrag);
+
+        // Call methods of the fragment
+        personDetailsFrag.setText("Details will appear here!");
+
     }
 }
